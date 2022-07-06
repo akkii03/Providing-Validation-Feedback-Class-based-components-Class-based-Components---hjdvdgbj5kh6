148c90d712975beea32d3d93bca586ae9cc53c0c
akkii03 <akkiindoria72@gmail.com>
2022-07-06 19:19:40 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,25 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
+		// this.state ={
+		// 	para:""
+		// }
+		const [dis,setDis] = useState("");
+		handler = ()=>{
+			setDis("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
+			console.log("btn was clicked");
+		}
 	};
+
+
 
     render() {
     	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
+			<div id="main">
+			<button id="click" onClick={this.handler}>click</button>
+			<p id="para">{dis}</p>
+			
+		</div>
     	);
     }
 }
